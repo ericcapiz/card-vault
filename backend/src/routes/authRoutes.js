9e128b46f9d934c1f1651a6f3d7372a6eb95bf22
eric capiz <ericcapiz@gmail.com>
2024-12-30 14:39:22 -0600

    authMiddleware

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../../models/users/User");
-const auth = require("../middleware/auth");
+const auth = require("../../middleware/auth");
 
 // Register
 router.post("/register", async (req, res) => {
